9914f478f07159afd2951f2061eb0fa48707ea43
Victor Hornets <vhornets@gmail.com>
2017-02-04 13:37:08 +0200

    Fix dashboard view

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,32 +1,26 @@
 @extends('admin.layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+<div class="panel panel-default">
+    <div class="panel-heading">Dashboard</div>
 
-                <div class="panel-body">
-                    <div class="list-group">
-                        <a href="{{url('/admin/releases')}}" class="list-group-item">
-                            Releases
-                        </a>
+    <div class="panel-body">
+        <div class="list-group">
+            <a href="{{url('/admin/releases')}}" class="list-group-item">
+                Releases
+            </a>
 
-                        <a href="{{ url('/admin/pages') }}" class="list-group-item">
-                            Pages
-                        </a>
+            <a href="{{ url('/admin/pages') }}" class="list-group-item">
+                Pages
+            </a>
 
-                        <a href="{{ url('/admin/menus') }}" class="list-group-item">
-                            Menus
-                        </a>
-                    
-                        <a href="{{ url('/admin/sociallinks') }}" class="list-group-item">
-                            Social links
-                        </a>
-                    </div>
-                </div>
-            </div>
+            <a href="{{ url('/admin/menus') }}" class="list-group-item">
+                Menus
+            </a>
+        
+            <a href="{{ url('/admin/sociallinks') }}" class="list-group-item">
+                Social links
+            </a>
         </div>
     </div>
 </div>
